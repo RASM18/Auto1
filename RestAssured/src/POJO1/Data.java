1b28cfca30c3842a93de147048474c51be7997e0
Vinod <Vinod@LAPTOP-FMURB739>
2023-02-07 14:26:56 +0530

    Review

---
+++ RestAssured/src/POJO1/Data.java
@@ -0,0 +1,32 @@
+package POJO1;
+
+public class Data 
+{
+	private int id ;
+	private String name;
+	private int year;
+	private String color;
+	private String pantone_value;
+	public int getId() 
+	{
+		return id;
+	}
+	public String getName() 
+	{
+		return name;
+	}
+	public int getYear() 
+	{
+		return year;
+	}
+	public String getColor() 
+	{
+		return color;
+	}
+	public String getPantone_value() 
+	{
+		return pantone_value;
+	}
+
+}
+
